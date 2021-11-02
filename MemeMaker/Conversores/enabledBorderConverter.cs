bcd2a4543cb66b4e2a3b14c74617672168e3ab03
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-11-02 14:55:56 +0100

    Acabo ejercicio

---
+++ MemeMaker/Conversores/enabledBorderConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class enabledBorderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int grosor = 2;
+
+            bool valor = (bool)value;
+
+            if (valor)
+            {
+                return grosor;
+            }
+            else
+            {
+                return 0;
+            }
+
+            
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
